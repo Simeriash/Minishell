4d3b258e4cf4213006794b6d973b6a9b2eecd987
julauren <249057325+Simeriash@users.noreply.github.com>
2026-02-28 11:37:29 +0100

    new header folder

--- Makefile
+++ Makefile
@@ -1,9 +1,9 @@
 NAME := minishell
 
 CC := cc
-CFLAGS := -Wall -Wextra -Werror -g # -fsanitize=address
+CFLAGS := -Wall -Wextra -Werror -g #-fsanitize=address
 
-HEADER := minishell.h
+HEADER := /inc
 
 LIBFT := src/libft/libft.a
 
@@ -20,7 +20,7 @@ OBJ := $(addprefix $(OBJ_DIR), $(SOURCES:.c=.o))
 all : $(NAME)
 
 $(NAME) : $(OBJ_DIR) $(OBJ)
-	$(MAKE) -C src/libft
+	# $(MAKE) -C src/libft
 	$(CC) $(CFLAGS) $(OBJ) -lreadline -o $@
 
 $(OBJ_DIR) :
@@ -30,12 +30,12 @@ $(OBJ_DIR)%.o : $(SRC_DIR)%.c
 	$(CC) $(CFLAGS) -I $(HEADER) -c $< -o $@
 
 clean :
-	$(MAKE) -C src/libft -B clean
+	# $(MAKE) -C src/libft -B clean
 	rm -rf $(OBJ)
 	rm -rf $(OBJ_DIR)
 
 fclean : clean
-	$(MAKE) -C src/libft -B fclean
+	# $(MAKE) -C src/libft -B fclean
 	rm -rf $(NAME)
 
 re : fclean all
--- inc/minishell.h
+++ inc/minishell.h
@@ -6,16 +6,17 @@
 /*   By: julauren <julauren@student.42angouleme.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/14 13:43:26 by julauren          #+#    #+#             */
-/*   Updated: 2026/02/18 14:16:52 by julauren         ###   ########.fr       */
+/*   Updated: 2026/02/28 09:21:41 by julauren         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
-# include "src/libft/libft.h"
+# include "../src/libft/libft.h"
 # include <stdio.h>
 # include <readline/readline.h>
 # include <readline/history.h>
+# include <stdlib.h>
 
 #endif
