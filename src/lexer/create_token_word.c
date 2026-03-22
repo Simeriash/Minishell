a82cff7fd1212715e6107daf2f747b6e6a41e703
julauren <249057325+Simeriash@users.noreply.github.com>
2026-03-22 09:20:22 +0100

    norminetted

--- src/lexer/create_token_word.c
+++ src/lexer/create_token_word.c
@@ -6,7 +6,7 @@
 /*   By: julauren <julauren@student.42angouleme.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/21 14:44:43 by julauren          #+#    #+#             */
-/*   Updated: 2026/03/21 15:45:17 by julauren         ###   ########.fr       */
+/*   Updated: 2026/03/22 07:57:29 by julauren         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,11 +48,7 @@ int	create_token_word(char *str, t_token *token_list, int *i, int *nb_token)
 	}
 	j = 0;
 	while (!end_condition(str[*i]))
-	{
-		value[j] = str[*i];
-		j++;
-		(*i)++;
-	}
+		value[j++] = str[(*i)++];
 	value[j] = '\0';
 	if (add_after(token_list, WORD, value))
 	{
