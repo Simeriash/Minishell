3ef679b4ebd33be04e0353e2a5938198da7ef028
julauren <249057325+Simeriash@users.noreply.github.com>
2026-03-27 15:42:47 +0100

    standby

--- src/parser/error.c
+++ src/parser/error.c
@@ -1,30 +1,24 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   parser.h                                           :+:      :+:    :+:   */
+/*   error.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: julauren <julauren@student.42angouleme.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/03/26 12:59:51 by julauren          #+#    #+#             */
-/*   Updated: 2026/03/26 15:54:49 by julauren         ###   ########.fr       */
+/*   Created: 2026/03/26 15:40:12 by julauren          #+#    #+#             */
+/*   Updated: 2026/03/27 14:36:57 by julauren         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PARSER_H
-# define PARSER_H
+#include "../../inc/parser.h"
+#include <unistd.h>
 
-# include "../src/libft/libft.h"
-# include "lexer.h"
-
-//	/!\		A CHANGER !!	(OU PAS)
-typedef struct s_env
+void	exit_parser(t_token *token_list, t_env *envc, t_error error)
 {
-	char			*key;
-	char			*value;
-	struct s_env	*next;
-}	t_env;
-
-int		expand(t_token *token_list, t_env *envc);
-void	exit_parser(t_token *token_list, t_env *envc, t_error error);
-
-#endif
+	(void)error;
+	(void)envc;
+	free_token(token_list);
+	// free_env(envc);
+	write(2, "Error parser\n", 14);
+	//	/!\		A REFAIRE
+}
--- src/parser/expand.c
+++ src/parser/expand.c
@@ -1,19 +1,40 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   parcer.c                                           :+:      :+:    :+:   */
+/*   expand.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: julauren <julauren@student.42angouleme.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/03/26 13:01:42 by julauren          #+#    #+#             */
-/*   Updated: 2026/03/26 15:54:49 by julauren         ###   ########.fr       */
+/*   Created: 2026/03/26 15:35:16 by julauren          #+#    #+#             */
+/*   Updated: 2026/03/26 16:43:32 by julauren         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../inc/parser.h"
 
-void	parser(t_token *token_list, t_env *envc)
+static t_error	expander(char *value, t_env *envc)
 {
-	if (expand(token_list, envc))
-		return ;
+	return (0);
+}
+
+int	expand(t_token *token_list, t_env *envc)
+{
+	t_token	*tmp;
+	t_error	error;
+
+	tmp = token_list->next;
+	while (tmp != NULL)
+	{
+		if (tmp->type == EXPAND)
+		{
+			error = expander(tmp->value, envc);
+			if (error)
+			{
+				exit_parser(token_list, envc, error);
+				return (1);
+			}
+		}
+		tmp = tmp->next;
+	}
+	return (0);
 }
