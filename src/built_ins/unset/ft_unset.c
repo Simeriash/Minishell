ce5034a95f0c89d2e05e15f402e52e41ff53dd57
Dancode52 <daniellanehart@gmail.com>
2026-03-31 11:08:07 +0200

    Edited function to unset multiple variables as args

--- src/built_ins/unset/ft_unset.c
+++ src/built_ins/unset/ft_unset.c
@@ -6,7 +6,7 @@
 /*   By: dlanehar <dlanehar@student.42angouleme.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/26 09:16:15 by dlanehar          #+#    #+#             */
-/*   Updated: 2026/03/26 10:59:49 by dlanehar         ###   ########.fr       */
+/*   Updated: 2026/03/31 11:05:52 by dlanehar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,30 +19,56 @@ static void	free_node(t_envpcpy *to_free)
 	free(to_free);
 }
 
-int	ft_unset(char *key, t_envpcpy **envpcpy)
+void	check_valid_var();
+
+static int	key_in_array(char *lookingfor, char **keys)
+{
+	int	i;
+
+	i = 0;
+	while (keys[i])
+	{
+		if (ft_strcmp(lookingfor, keys[i]) == 0)
+			return (1);
+		i++;
+	}
+	return (0);
+}
+static	void unlink_node(t_envpcpy *tmp, t_envpcpy *prev, t_envpcpy **envpcpy)
+{
+	t_envpcpy *next;
+
+	next = tmp->next;
+	if (!prev)
+		*envpcpy = next;
+	else
+		prev->next = next;
+	free_node(tmp);
+}
+
+int	ft_unset(char **key, t_envpcpy **envpcpy)
 {
 	t_envpcpy	*tmp;
 	t_envpcpy	*prev;
-	int			len;
+	t_envpcpy	*next;
 
 	if (!key || !envpcpy)
 		return (-1);
 	tmp = *envpcpy;
 	prev = NULL;
-	len = ft_strlen(key);
 	while (tmp)
 	{
-		if (ft_strncmp(tmp->key, key, len) == 0 && tmp->key[len] == '\0')
+		if (key_in_array(tmp->key, key))
 		{
-			if (prev == NULL) //this means that we found key in the head node.
-				*envpcpy = tmp->next;
-			else
-				prev->next = tmp->next;
-			free_node(tmp);
-			return (0);
+			next = tmp->next;
+			unlink_node(tmp, prev, envpcpy);
+			tmp = next;
 		}
-		prev = tmp;
-		tmp = tmp->next;
+		else
+		{
+			prev = tmp;
+			tmp = tmp->next;
+		}
 	}
 	return (-1);
 }
