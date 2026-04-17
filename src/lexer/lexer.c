75cdb77bf0d625daeaa463b9926c30405c7a54c6
julauren <249057325+Simeriash@users.noreply.github.com>
2026-04-17 10:22:38 +0200

    an other lexer correction and optimization

--- src/lexer/lexer.c
+++ src/lexer/lexer.c
@@ -6,27 +6,18 @@
 /*   By: julauren <julauren@student.42angouleme.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/13 09:38:57 by julauren          #+#    #+#             */
-/*   Updated: 2026/04/14 12:43:47 by julauren         ###   ########.fr       */
+/*   Updated: 2026/04/17 10:16:49 by julauren         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../inc/lexer.h"
 
-static t_ret	lexer_next3(t_token **last, char *s)
+static int	looking_for_string(char *str, char **s, int *i)
 {
-	if (add_after(*last, WORD, s))
-	{
-		error_lexer(MALLOC);
-		return (NUL);
-	}
-	*last = (*last)->next;
-	return (0);
-}
-
-static t_ret	lexer_next2(char *str, char **s, int *i, int start)
-{
+	int		start;
 	t_state	state;
 
+	start = *i;
 	state = NORMAL;
 	while (str[*i] != '\0' && ((state == NORMAL && !end_condition(str[*i]))
 			|| state == SIMPLE_QUOTE || state == DOUBLE_QUOTE))
@@ -34,31 +25,22 @@ static t_ret	lexer_next2(char *str, char **s, int *i, int start)
 	if (state != NORMAL)
 	{
 		error_lexer(OPEN_QUOTE);
-		return (NUL);
+		return (1);
 	}
 	*s = ft_substr(str, start, *i - start);
 	if (!(*s))
 	{
 		error_lexer(MALLOC);
-		return (NUL);
-	}
-	if (((*s)[0] == '\'' && (*s)[1] == '\'')
-		|| ((*s)[0] == '"' && (*s)[1] == '"'))
-	{
-		free(*s);
-		return (CONTINUE);
+		return (1);
 	}
 	return (0);
 }
 
-static t_ret	lexer_next(char *str, t_token **last, int *i)
+static int	create_token(char *str, t_token **last, int *i)
 {
 	t_error	error;
-	t_ret	ret;
 	char	*s;
-	int		start;
 
-	start = *i;
 	if (str[*i] == '<' || str[*i] == '>' || str[*i] == '|' || str[*i] == '&'
 		|| str[*i] == '(' || str[*i] == ')' || str[*i] == '\n')
 	{
@@ -66,22 +48,25 @@ static t_ret	lexer_next(char *str, t_token **last, int *i)
 		if (error)
 		{
 			error_lexer(error);
-			return (NUL);
+			return (1);
 		}
 		*last = (*last)->next;
-		return (CONTINUE);
+		return (0);
 	}
-	ret = lexer_next2(str, &s, i, start);
-	if (ret != 0)
-		return (ret);
-	ret = lexer_next3(last, s);
-	return (ret);
+	if (looking_for_string(str, &s, i))
+		return (1);
+	if (add_after(*last, WORD, s))
+	{
+		error_lexer(MALLOC);
+		return (1);
+	}
+	*last = (*last)->next;
+	return (0);
 }
 
 t_token	*lexer(char *str)
 {
 	int		i;
-	t_ret	ret;
 	t_token	*token_list;
 	t_token	*last;
 
@@ -95,10 +80,7 @@ t_token	*lexer(char *str)
 		shell_space(str, &i);
 		if (str[i] == '\0')
 			break ;
-		ret = lexer_next(str, &last, &i);
-		if (ret == BREAK)
-			break ;
-		if (ret == NUL)
+		if (create_token(str, &last, &i))
 		{
 			free_token(token_list);
 			return (NULL);
