2383e65721b62b23639ab4230eb05100b549b67d
julauren <249057325+Simeriash@users.noreply.github.com>
2026-05-16 09:32:18 +0200

    try to expand

--- inc/heredoc.h
+++ inc/heredoc.h
@@ -6,7 +6,7 @@
 /*   By: julauren <julauren@student.42angouleme.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/14 12:27:04 by julauren          #+#    #+#             */
-/*   Updated: 2026/05/14 12:28:46 by julauren         ###   ########.fr       */
+/*   Updated: 2026/05/16 09:08:52 by julauren         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,15 @@
 # define HEREDOC_H
 
 # include "../src/libft/libft.h"
+# include "parser.h"
+
+# include <stdio.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+# include <fcntl.h>
+
+int	heredoc(char *eof, t_env *envc);		//	/!\ A EFFACER
 
 #endif
