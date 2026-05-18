9aa4f5b81d80d73c931e314327395ba275ad03da
Dancode52 <daniellanehart@gmail.com>
2026-05-18 10:27:32 +0200

    Normed pwd

--- src/built_ins/pwd/ft_pwd.c
+++ src/built_ins/pwd/ft_pwd.c
@@ -6,7 +6,7 @@
 /*   By: dlanehar <dlanehar@student.42angouleme.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/24 12:59:16 by dlanehar          #+#    #+#             */
-/*   Updated: 2026/05/13 15:55:48 by dlanehar         ###   ########.fr       */
+/*   Updated: 2026/05/18 10:27:12 by dlanehar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,21 +14,29 @@
 
 int	ft_pwd(t_envpcpy **envpcpy)
 {
+	char	*cwd;
 	char	*w_dir;
+	int		ret_val;
 
-	w_dir = getcwd(NULL, 0);
-	if (!w_dir)
+	cwd = getcwd(NULL, 0);
+	if (!cwd)
 	{
 		w_dir = find_env_var("PWD", envpcpy);
 		if (!w_dir)
 		{
-			printf("PWD is unset\n");
+			ret_val = write(2, "PWD is unset\n", 14);
+			if (ret_val < 0)
+				return (ret_val);
 			return (-1);
 		}
-		printf("%s\n", w_dir);
+		ret_val = printf("%s\n", w_dir);
+		if (ret_val < 0)
+			return (ret_val);
 		return (0);
 	}
-	printf("%s\n", w_dir);
-	free(w_dir);
+	ret_val = printf("%s\n", cwd);
+	if (ret_val < 0)
+		return (ret_val);
+	free(cwd);
 	return (0);
 }
