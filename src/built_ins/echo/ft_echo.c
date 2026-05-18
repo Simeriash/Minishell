ad27f5b9bc8d2877abf08603176b3b32f31c9e24
Dancode52 <daniellanehart@gmail.com>
2026-05-18 10:08:58 +0200

    Add option parsing and normed

--- src/built_ins/echo/ft_echo.c
+++ src/built_ins/echo/ft_echo.c
@@ -6,66 +6,77 @@
 /*   By: dlanehar <dlanehar@student.42angouleme.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/20 15:27:58 by dlanehar          #+#    #+#             */
-/*   Updated: 2026/05/13 15:37:05 by dlanehar         ###   ########.fr       */
+/*   Updated: 2026/05/18 10:08:17 by dlanehar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "builtins.h"
 
-int	check_for_nl(char *args)
+static int	check_dash_n(char *args)
 {
-	int i;
-	int	print_nl;
+	int	i;
 
 	i = 0;
-	print_nl = 1;
-	if (args[i] == '-')
+	if (!args || args[i] != '-')
+		return (0);
+	i++;
+	if (args[i] == '\0')
+		return (0);
+	while (args[i])
 	{
+		if (args[i] != 'n')
+			return (0);
 		i++;
-		while (args[i])
-		{
-			if (args[i] == 'n')
-				print_nl = 0;
-			else
-			{
-				print_nl = 1;
-				break ;
-			}
-			i++;
-		}
 	}
-	return (print_nl);
+	return (1);
+}
+
+static int	print_words(int index, char **args, int nl_flag)
+{
+	int	ret_val;
+
+	ret_val = 0;
+	while (args[index])
+	{
+		ret_val = printf("%s", args[index]);
+		if (ret_val < 0)
+			return (ret_val);
+		if (args[index + 1])
+		{
+			ret_val = printf(" ");
+			if (ret_val < 0)
+				return (ret_val);
+		}
+		index++;
+	}
+	if (nl_flag)
+	{
+		ret_val = printf("\n");
+		if (ret_val < 0)
+			return (ret_val);
+	}
+	return (ret_val);
 }
 
 int	ft_echo(char *path, char **args, t_envpcpy **envpcpy)
 {
 	int	i;
 	int	ret_val;
-	int	print_nl;
+	int	nl_flag;
 
 	(void)path;
 	(void)envpcpy;
 	i = 0;
-	ret_val = 0;
+	nl_flag = 1;
 	if (!args)
 		return (1);
-	if (ft_strcmp("echo", args[i]) == 0)
-		i++;
-	print_nl = check_for_nl(args[i]);
-		i++;
-	while (args[i])
+	while (args[i] && check_dash_n(args[i]))
 	{
-		//check if first arg is a -n or any combo of ns. if a non n appears, treat as a word
-
-		ret_val = printf("%s", args[i]);
-		if (!ret_val)
-			return (-1);
-		if (args[i + 1])
-			ret_val = printf(" ");
 		i++;
+		nl_flag = 0;
 	}
-	if (print_nl)
-		ret_val = printf("\n");
+	ret_val = print_words(i, args, nl_flag);
+	if (ret_val < 0)
+		return (ret_val);
 	return (0);
 }
-
