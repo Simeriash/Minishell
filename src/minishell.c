a2b9618183bf4df1b91411a470926c910f4a4c87
Dancode52 <daniellanehart@gmail.com>
2026-05-27 15:52:39 +0200

    Changed main to pass &envc instead of envc to execute_tree

--- src/minishell.c
+++ src/minishell.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: julauren <julauren@student.42angouleme.    +#+  +:+       +#+        */
+/*   By: dlanehar <dlanehar@student.42angouleme.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/18 13:09:01 by julauren          #+#    #+#             */
-/*   Updated: 2026/05/27 15:30:42 by julauren         ###   ########.fr       */
+/*   Updated: 2026/05/27 15:39:08 by dlanehar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -67,7 +67,7 @@ int main(int argc, char **argv, char **envp)
 		ast = parser(token, envc);
 		if (!ast)
 			continue ;
-		execute_tree(ast, envp, STDIN_FILENO, STDOUT_FILENO);
+		execute_tree(ast, &envc, STDIN_FILENO, STDOUT_FILENO);
 		free_ast(ast);
 	}
 	ft_free_envc(envc);
