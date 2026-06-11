11169c5777026f757e23ab2ae86d0dacd5a3642f
Dancode52 <daniellanehart@gmail.com>
2026-06-11 09:58:34 +0200

    Comment cleanup and return value normalisation

--- src/built_ins/export/ft_export.c
+++ src/built_ins/export/ft_export.c
@@ -6,7 +6,7 @@
 /*   By: dlanehar <dlanehar@student.42angouleme.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/26 09:16:00 by dlanehar          #+#    #+#             */
-/*   Updated: 2026/05/27 12:10:54 by dlanehar         ###   ########.fr       */
+/*   Updated: 2026/06/11 09:57:35 by dlanehar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,86 +20,86 @@ int	check_valid_input(char *in)
 	if (!(in[i] == '_' || ft_isalpha(in[i])))
 	{
 		printf("bash: export: `%s': not a valid identifier\n", in);
-		return (-1);
+		return (1);
 	}
 	i++;
 	while (in[i] && in[i] != '=')
 	{
 		if (in[i] == '+' && in[i + 1] == '=')
 			return (0);
-		if (cvi_error_check(in, i) < 0)
-			return (-1);
+		if (cvi_error_check(in, i) != 0)
+			return (1);
 		i++;
 	}
 	return (0);
 }
 
-static int	set_key_and_value(char *arg, char **key, char **value) // FROM INT TO T_ERRORS
+static int	set_key_and_value(char *arg, char **key, char **value)
 {
-	int	error; // BECOME T_ERRORS ERROR
+	int	error;
 
 	*key = NULL;
 	*value = NULL;
 	error = get_key(arg, key);
-	if (error < 0)
-		return (-1); // RETURN ERROR
+	if (error != 0)
+		return (1);
 	error = get_value(arg, value);
-	if (error < 0)
+	if (error != 0)
 	{
 		free(*key);
 		*key = NULL;
-		return (-1); // RETURN ERROR
+		return (1);
 	}
-	return (0); // RETURN SUCCESS (HELD IN ERROR)
+	return (0);
 }
 
-static int	process_input(char *arg, t_env **envpcpy) // FROM INTO TO T_ERRORS
+static int	process_input(char *arg, t_env **envpcpy)
 {
 	char		*key;
 	char		*value;
 	t_env_mode	mode;
-	int			error; // BECOME T_ERRORS ERROR
+	int			error;
 
 	mode = ENV_REPLACE;
 	key = NULL;
 	value = NULL;
 	error = set_key_and_value(arg, &key, &value);
-	if (error < 0)
-		return (-1); // RETURN ERROR
+	if (error != 0)
+		return (1);
 	if (must_append(arg))
 		mode = ENV_APPEND;
 	env_set(key, value, envpcpy, mode);
 	free(key);
 	free(value);
-	if (error < 0)
-		return (-1); // RETURN ERROR
-	return (0); // SUCCESS
+	if (error != 0)
+		return (1);
+	return (0);
 }
 
-int	ft_export(char **args, t_env **envpcpy) // FROM INT TO T_ERRORS
+int	ft_export(char **args, t_env **envpcpy)
 {
 	int			i;
 	int			error;
 
-	i = 0;
+	i = 1;
 	if (!args)
-		return (-1); // NULL ARG
+		return (1);
 	if (!args[i])
 	{
-		print_env_in_alpha_order(envpcpy); // ERROR =
-		return (0); // RETURN ERROR
+		print_env_in_alpha_order(envpcpy);
+		return (0);
 	}
 	while (args[i])
 	{
-		if (check_valid_input(args[i]) < 0)
+		if (check_valid_input(args[i]) != 0)
 		{
 			i++;
 			continue ;
 		}
 		error = process_input(args[i], envpcpy);
-		if (error < 0)
-			return (-1); // RETURN ERROR
+		if (error != 0)
+			return (1);
 		i++;
 	}
-	return (0); // SUCCESS
+	return (0);
 }
