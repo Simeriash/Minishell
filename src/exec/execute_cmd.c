de62ca7b2ddebcbbbccbf302a512bb8794565f0d
Dancode52 <daniellanehart@gmail.com>
2026-06-16 15:53:25 +0200

    Correcting handling of direct paths

--- src/exec/execute_cmd.c
+++ src/exec/execute_cmd.c
@@ -3,14 +3,27 @@
 /*                                                        :::      ::::::::   */
 /*   execute_cmd.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: julauren <julauren@student.42angouleme.    +#+  +:+       +#+        */
+/*   By: dlanehar <dlanehar@student.42angouleme.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/27 12:41:20 by dlanehar          #+#    #+#             */
-/*   Updated: 2026/06/15 11:02:11 by julauren         ###   ########.fr       */
+/*   Updated: 2026/06/16 15:38:29 by dlanehar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../inc/execute.h"
+
+static	char *copy_direct_path(char *cmd, t_exec_err *err)
+{
+	char *ret;
+
+	ret = ft_strdup(cmd);
+	if (!ret)
+	{
+		*err = EXEC_MALLOC_FAIL;
+		return (NULL);
+	}
+	return (ret);
+}
 
 char	*find_executable(char *cmd, t_exec_err *err, t_env **env)
 {
@@ -19,40 +32,25 @@ char	*find_executable(char *cmd, t_exec_err *err, t_env **env)
 
 	*err = EXEC_OK;
 	if (!cmd || !*cmd)
-	{
-		*err = EXEC_NOT_FOUND;
 		return (NULL);
-	}
 	if (ft_strchr(cmd, '/'))
 	{
-		ret = ft_strdup(cmd);
-		if (!ret)
-		{
-			*err = EXEC_MALLOC_FAIL;
-			return (NULL);
-		}
+		ret = copy_direct_path(cmd, err);
 		return (ret);
 	}
 	paths = create_paths(err, env);
 	if (!paths)
-		return (NULL);
-	ret = create_candidate(cmd, paths, err);
-	free_array(paths);
-	return (ret);
-}
-
-char	*handle_executable(t_ast *node, t_env **env)
-{
-	t_exec_err	err;
-	char		*exe;
-
-	exe = find_executable(node->cmd->args[0], &err, env);
-	if (!exe)
 	{
-		ft_printf_fd(2, "Ghost: %s: command not found\n", node->cmd->args[0]);
-		return (NULL);
+		if (*err == EXEC_MALLOC_FAIL)
+			return (NULL);
+		ret = ft_strdup(cmd);
 	}
-	return (exe);
+	else
+	{
+		ret = create_candidate(cmd, paths, err);
+		free_array(paths);
+	}
+	return (ret);
 }
 
 int	handle_builtin(t_ast *node, t_env **env, t_fds *fds)
@@ -69,11 +67,17 @@ int	handle_builtin(t_ast *node, t_env **env, t_fds *fds)
 	return (-1);
 }
 
-int	prep_for_execve(t_ast *node, t_env **env, char **exec)
+int	prep_command(t_ast *node, t_env **env, char **exec)
 {
-	*exec = handle_executable(node, env);
+	t_exec_err	err;
+
+	*exec = find_executable(node->cmd->args[0], &err, env);
 	if (!*exec)
+	{
+		ft_printf_fd(2, "Ghost: %s: command not found\n", node->cmd->args[0]);
+		*exec = NULL;
 		return (127);
+	}
 	return (0);
 }
 
@@ -88,7 +92,7 @@ int	execute_cmd(t_ast *node, t_env **envp, t_fds *fds)
 	error = handle_builtin(node, envp, fds);
 	if (error != -1)
 		return (error);
-	error = prep_for_execve(node, envp, &executable);
+	error = prep_command(node, envp, &executable);
 	if (error)
 		return (error);
 	status = run_command(node, fds, executable, envp);
