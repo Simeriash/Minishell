4abe27e8517ec5d11002dd411302fb574952d0f5
Dancode52 <daniellanehart@gmail.com>
2026-06-16 09:55:36 +0200

    Added long long value handling

--- src/built_ins/exit/ft_exit.c
+++ src/built_ins/exit/ft_exit.c
@@ -3,36 +3,58 @@
 /*                                                        :::      ::::::::   */
 /*   ft_exit.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: julauren <julauren@student.42angouleme.    +#+  +:+       +#+        */
+/*   By: dlanehar <dlanehar@student.42angouleme.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/20 15:50:55 by dlanehar          #+#    #+#             */
-/*   Updated: 2026/06/15 10:49:15 by julauren         ###   ########.fr       */
+/*   Updated: 2026/06/16 09:54:47 by dlanehar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../../inc/builtins.h"
+#include "../../../inc/error.h"
 
-// static int	ft_longatoi(const char *str, int *ctrl)
-// {
-// }
-
-static void	free_env(t_env **delete)
+static long long	create_num(unsigned long long limit, const char *str,
+								int *ctrl)
 {
-	t_env	*tmp;
-	t_env	*next;
+	unsigned long long	num;
+	int					i;
 
-	tmp = *delete;
-	while (tmp)
+	i = 0;
+	num = 0;
+	while (str[i])
 	{
-		next = tmp->next;
-		if (tmp->key)
-			free(tmp->key);
-		if (tmp->value)
-			free(tmp->value);
-		free(tmp);
-		tmp = next;
+		if (num > (limit - (str[i] - '0')) / 10)
+		{
+			num = 0;
+			*ctrl = -1;
+			return (0);
+		}
+		num = num * 10 + (str[i] - '0');
+		i++;
 	}
-	return ;
+	return (num);
+}
+
+static long long	ft_longatoi(const char *str, int *ctrl)
+{
+	int					i;
+	int					sign;
+	unsigned long long	num;
+	unsigned long long	limit;
+
+	i = 0;
+	num = 0;
+	sign = 1;
+	limit = LLONG_MAX;
+	if (str[i] == '-')
+	{
+		sign = -sign;
+		limit = (unsigned long long)LLONG_MAX + 1;
+	}
+	if (str[i] == '-' || str[i] == '+')
+		i++;
+	num = create_num(limit, str + i, ctrl);
+	return ((long long)sign * num);
 }
 
 static int	check_alpha(char *str)
@@ -69,7 +91,7 @@ int	ft_exit(char **args, t_env **delete)
 	if (count_args(args) > 2)
 	{
 		write(2, "Ghost: exit: too many arguments", 31);
-		free_env(delete);
+		ft_free_envc(*delete);
 		return (1);
 	}
 	if (args[1])
@@ -77,11 +99,12 @@ int	ft_exit(char **args, t_env **delete)
 		if (check_alpha(args[1]))
 			exit_value = 2;
 		else
-			exit_value = ft_atoi(args[1], &ctrl);
+			exit_value = ft_longatoi(args[1], &ctrl);
 	}
 	if (ctrl != 0)
 		exit_value = 2;
-	free_env(delete);
+	ft_free_envc(*delete);
 	rl_clear_history();
+	write(1, "exit\n", 6);
 	return (exit_value);
 }
