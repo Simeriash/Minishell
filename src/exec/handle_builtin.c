78004fab0f7bd0b6a1ec852ad0ebab32d1a631b5
Dancode52 <daniellanehart@gmail.com>
2026-06-17 09:41:30 +0200

    Added really ugly exit condition

--- src/exec/handle_builtin.c
+++ src/exec/handle_builtin.c
@@ -6,13 +6,53 @@
 /*   By: dlanehar <dlanehar@student.42angouleme.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/12 11:17:30 by dlanehar          #+#    #+#             */
-/*   Updated: 2026/06/15 10:06:36 by dlanehar         ###   ########.fr       */
+/*   Updated: 2026/06/17 09:36:19 by dlanehar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../inc/builtins.h"
 #include "../../inc/execute.h"
 #include "../../inc/parser.h"
+
+static void	clean_exit(t_env **env, t_ast *node, t_fds *fds, int status)
+{
+	if (fds->fd_in != STDIN_FILENO)
+		close(fds->fd_in);
+	if (fds->fd_out != STDOUT_FILENO)
+		close(fds->fd_out);
+	free_ast(node->ast);
+	ft_free_envc(*env);
+	exit(status);
+}
+
+static int	dont_exit(char **args)
+{
+	int	i;
+	int	j;
+	int	ctrl;
+
+	ctrl = 0;
+	i = 0;
+	j = 0;
+	if (args[1])
+	{
+		while (args[1][i])
+		{
+			if (ft_isdigit(args[1][i]))
+				i++;
+			else
+				return (0);
+		}
+		ft_longatoi(args[1], &ctrl);
+		if (ctrl == -1)
+			return (0);
+		while (args[j])
+			j++;
+	}
+	if (j > 2)
+		return (1);
+	return (0);
+}
 
 t_builtin_func	get_builtin(char **args, t_env **envpcpy)
 {
@@ -56,12 +96,8 @@ int	execute_builtin(t_env **env, t_ast *node, t_fds *fds, t_builtin_func func)
 	close_redirects(fds);
 	if (ft_strcmp("exit", node->cmd->args[0]) == 0)
 	{
-		free_ast(node->ast);
-		if (fds->fd_in != STDIN_FILENO)
-			close(fds->fd_in);
-		if (fds->fd_out != STDOUT_FILENO)
-			close(fds->fd_out);
-		exit(status);
+		if (dont_exit(node->cmd->args) == 0)
+			clean_exit(env, node, fds, status);
 	}
 	return (status);
 }
