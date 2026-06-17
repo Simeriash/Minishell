48744e3214a6598026a2ed63593b57f1127524eb
Dancode52 <daniellanehart@gmail.com>
2026-06-17 15:14:19 +0200

    Added behaviour for being within deleted folder

--- src/built_ins/cd/ft_cd.c
+++ src/built_ins/cd/ft_cd.c
@@ -6,7 +6,7 @@
 /*   By: dlanehar <dlanehar@student.42angouleme.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/20 14:55:04 by dlanehar          #+#    #+#             */
-/*   Updated: 2026/06/17 08:00:25 by dlanehar         ###   ########.fr       */
+/*   Updated: 2026/06/17 14:52:30 by dlanehar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,16 +32,28 @@ static int	update_oldpwd_pwd(char *old, char *new, t_env **envpcpy)
 	return (ret_val);
 }
 
-static int	chdir_success(char **current, char *old, t_env **envpcpy)
+static int	chdir_success(char **current, char **old, t_env **envpcpy)
 {
 	int	ret_val;
 
 	ret_val = 0;
+
+	if (!*old)
+	{
+		*old = ft_strdup("");
+		if (!*old)
+			return (1);
+	}
 	*current = getcwd(NULL, 0);
 	if (!*current)
-		return (1);
-	ret_val = update_oldpwd_pwd(old, *current, envpcpy);
-	if (ret_val < 0)
+	{
+		ft_printf_fd(2, "cd: error retrieving current directory: getcwd: ");
+		ft_printf_fd(2, "cannot access parent directories: ");
+		ft_printf_fd(2, "%s\n", strerror(errno));
+		return (0);
+	}
+	ret_val = update_oldpwd_pwd(*old, *current, envpcpy);
+	if (ret_val != 0)
 		return (1);
 	return (ret_val);
 }
@@ -55,11 +67,9 @@ static int	cd_executor(char *cd_input, t_env **envpcpy)
 
 	current_path = NULL;
 	old_path = getcwd(NULL, 0);
-	if (!old_path)
-		return (1);
 	ret_val = chdir(cd_input);
 	if (ret_val == 0)
-		ret_val = chdir_success(&current_path, old_path, envpcpy);
+		ret_val = chdir_success(&current_path, &old_path, envpcpy);
 	else
 	{
 		err = errno;
@@ -94,11 +104,18 @@ int	ft_cd(char **args, t_env **envpcpy)
 {
 	int		ret_val;
 
-	if (!args[1])
+	if (count_args(args) > 2)
+	{
+		write(2, "Ghost: cd: too many arguments\n", 31);
+		return (1);
+	}
+	if (!args[1] || (args[1] && ft_strcmp("--", args[1]) == 0))
 	{
 		ret_val = cd_to_home(envpcpy);
 		return (ret_val);
 	}
+	if (args[1][0] == '\0')
+		return (0);
 	ret_val = cd_executor(args[1], envpcpy);
 	return (ret_val);
 }
